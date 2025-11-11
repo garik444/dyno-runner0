691c2076cf430d94a26b6136d6b7b09eb62b9a45
garik444 <gghevondyan21@gmail.com>
2025-11-11 10:01:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-
-document.addEventListener("keydown", function (event) {
-  if (event.code === "Space") {
-    jump();
-  }
-});
-
-function jump() {
-  if (dino.classList != "jump") {
-    dino.classList.add("jump");
-    setTimeout(function () {
-      dino.classList.remove("jump");
-    }, 500);
-  }
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f7f7f7;
+  overflow: hidden;
+  font-family: Arial, sans-serif;
 }
 
-let isAlive = setInterval(function () {
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-  let cactusLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left")
-  );
+.game {
+  width: 600px;
+  height: 200px;
+  border-bottom: 2px solid #000;
+  position: relative;
+  background-color: #fff;
+  overflow: hidden;
+}
 
-  if (cactusLeft < 70 && cactusLeft > 0 && dinoTop >= 140) {
-    alert("Game Over!");
-  }
-}, 10);
+#dino {
+  width: 40px;
+  height: 40px;
+  background-color: #444;
+  position: absolute;
+  bottom: 0;
+  left: 50px;
+}
+
+.cactus {
+  width: 25px;
+  height: 40px;
+  background-color: green;
+  position: absolute;
+  bottom: 0;
+  right: -30px;
+}
+
+#score {
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.jump {
+  animation: jump 0.5s linear;
+}
+
+@keyframes jump {
+  0% { bottom: 0; }
+  50% { bottom: 80px; }
+  100% { bottom: 0; }
+}
+
